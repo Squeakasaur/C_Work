cb0a57acde3dce63b178051b0a12b3fac949a392
Squeakasaur <Melissamariej@gmail.com>
2014-03-07 21:30:28 +0000

    Updated formatting

--- hatescale.c
+++ hatescale.c
@@ -2,28 +2,33 @@
 
 #include <stdio.h>
 
+
 int main(void)
 {
-char x;
+	char x;
 
-printf("Is it the weekend?");
-scanf("%c", &x);
-  if(x=='y' || x=='Y')
-  {
-  printf("He hates you alot\n"); 
+
+	printf("Is it the weekend?");
+	scanf("%c", &x);
+	if(x=='y' || x=='Y')
+	{
+		printf("He hates you alot\n"); 
+	}
+
+	else if
+		(x=='n' || x=='N')
+		{
+			scanf("%c", &x);
+			printf("Is it early morning?\n");
+		}
+	else
+		(x=='a' || x=='A')
+			;
+	{
+		printf("Please respond with Y or N\n");
+
+	} 
 }
 
-  else if
-  (x=='n' || x=='N')
-{
-  scanf("%c", &x);
-  printf("Is it early morning?\n");
-}
-else
-(x=='a' || x=='A')
-;
-{
-printf("Please respond with Y or N\n");
-} 
 
-}
+
