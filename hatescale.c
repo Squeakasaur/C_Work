f4e02d0913ac007257b1292e27abb9008c8f1a46
Squeakasaur <Melissamariej@gmail.com>
2014-03-07 06:04:58 +0000

    Matt's Hate Scale

---
+++ hatescale.c
@@ -0,0 +1,12 @@
+/* how  much does matt hate me right now */
+
+#include <stdio.h>
+
+int main(void)
+{
+char x;
+
+printf("Is it the weekend?");
+scanf("%c", &x);
+printf("It doesn't matter when it is, he always hates you!\n");
+}
