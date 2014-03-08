56293735243b21fdb538e3c1568b5a5667887179
Squeakasaur <Melissamariej@gmail.com>
2014-03-08 06:54:17 +0000

    Finally got my code to work :D

--- hatescale.c
+++ hatescale.c
@@ -1,31 +1,67 @@
 /* how  much does matt hate me right now */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-
+char read_char(char *);
 int main(void)
 {
 	char x;
 
+	while(1)
+	{
 
-	printf("Is it the weekend?");
-	scanf("%c", &x);
-	if(x=='y' || x=='Y')
-	{
-		printf("He hates you alot\n"); 
-	}
+		x=read_char("Is it the weekend? ");
+		if(tolower(x)=='y')
+		{
+			printf("He hates you alot\n"); 
+			break;
+		}     
 
-	else if
-	(x=='n' || x=='N')
-	{
-		scanf("%c", &x);
-		printf("Is it early morning?\n");
+		else if	(tolower(x)=='n')
+		{
+			while(1)
+			{
+				x=read_char("Is it early morning? ");
+				if(tolower(x)=='y')
+				{
+					printf("He probably wants to kill you\n");
+					exit(0);
+				}
+				else if (tolower(x)=='n')
+				{ 
+					printf("Doesn't matter, he still hates you!\n");
+					exit(0);
+				} 
+				else
+				{
+					printf("Please respond with Y or N\n");
+				    break;
+				}
+			}
+		}
+		else if(tolower(x)=='q')
+		{
+			exit(0);
+		}
+		else
+		{
+			printf("Please respond with Y or N\n");
+		}
 	}
-	else
-	{
-		printf("Please respond with Y or N\n");
-	} 
 }
 
 
+char read_char (char * message)
+{
+	char ch;
+	printf("%s ", message);
+	scanf("%c", &ch);
+
+	while(fgetc(stdin)!='\n')
+	{}
+	
+	return ch; 
+}
 
