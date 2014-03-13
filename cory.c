5057e19823d816453cc8fd4002dda6ccfc02372b
Squeakasaur <Melissamariej@gmail.com>
2014-03-13 23:43:30 +0000

    Cory<3

---
+++ cory.c
@@ -0,0 +1,42 @@
+/* Love Scale, opposite of hatescale for Cory <3 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+	char read_char(char *); 
+	char c;
+	while(1){
+
+		c=read_char("Is his name cory?");
+		{
+			if(tolower(c)=='y'){
+			printf("So much love it hurts\n");
+				exit(0);
+				}
+
+			else if(tolower(c)=='n')
+			{
+				printf("who the hell are you?this is Cory's love scale >.>\n");
+				exit(0);
+
+			}
+
+		}
+
+	}
+
+}
+char read_char (char * message)
+{
+	char ch;
+	printf("%s ", message);
+	scanf("%c", &ch);
+
+	while(fgetc(stdin)!='\n')
+	{}
+	return ch;
+}
+
+
